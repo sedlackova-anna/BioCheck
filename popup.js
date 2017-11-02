e9ac1b4862eb916c3171522817bd2ed7c6ce9f6a
Anna Sedlackova <axs1202@case.edu>
2017-11-02 11:59:12 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,15 +1,3 @@
-/*
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    
-    document.getElementById("author").innerHTML = request.author;
-    document.getElementById("description").innerHTML = request.deets;
-    
-    sendResponse("succ ess");
-
-  }
-);
-*/
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, "gimmie", function(response) {
